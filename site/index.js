f089586fde38355839da5794a9386fd8ac7c8ab8
Blair Bristow <blairhbristow@gmail.com>
2022-04-05 12:31:04 -0400

    Add pictures

--- site/index.js
+++ site/index.js
@@ -1,6 +1,12 @@
 const api = "https://swapi.dev/api/films/"
 const spinner = document.querySelector(".spinner")
 const movieListings = document.querySelector(".movie-listings")
+const newHope = "http://www.omdbapi.com/?t=star+wars&apikey=b5743b67"
+const empire = "http://www.omdbapi.com/?t=empire+strikes+back&apikey=b5743b67"
+const returnOfTheJedi = "http://www.omdbapi.com/?t=return+of+the+jedi&apikey=b5743b67"
+const phantom = "http://www.omdbapi.com/?t=phantom+menace&apikey=b5743b67"
+const clones = "http://www.omdbapi.com/?t=attack+of+the+clones&apikey=b5743b67"
+const revenge = "http://www.omdbapi.com/?t=revenge+of+the+sith&apikey=b5743b67"
 
 
 function addFilm(film) {
@@ -13,6 +19,12 @@ function addFilm(film) {
     movieListings.append(div)
 }
 
+function addPicture(film) {
+    const img = document.createElement("img")
+    img.src = `${film.Poster}`
+    movieListings.append(img)
+}
+
 fetch(api)
     .then(response => {
         return response.json()
@@ -21,9 +33,24 @@ fetch(api)
         const fetches = urls.map(url => fetch(url).then(response => response.json()))
         return Promise.all(fetches)
     }).then(responses => {
-        console.log(responses)
         spinner.classList.add("hidden")
         responses.forEach(response => {
             addFilm(response)
         })
+    })
+
+fetch(newHope)
+    .then(response => {
+        return response.json()
+    }).then(response => {
+        console.log(response)
+        addPicture(response)
+    })
+
+fetch(empire)
+    .then(response => {
+        return response.json()
+    }).then(response => {
+        console.log(response)
+        addPicture(response)
     })
