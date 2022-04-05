4b43b8af133a2e72eb21a4c8018c99f3ec9af0b9
Blair Bristow <blairhbristow@gmail.com>
2022-04-05 15:26:27 -0400

    Add posters to api

--- site/movie.js
+++ site/movie.js
@@ -1,19 +1,21 @@
 const url = new URL(window.location)
 const query = new URLSearchParams(url.search)
-const swapi = "https://swapi.dev/api/films/"
+const spinner = document.querySelector(".spinner")
+const movieDiv = document.querySelector(".movie")
+const ul = document.querySelector(".characters")
+const api = "https://swapi.dev/api/films/"
 
-
-
-
-fetch(`https://swapi.dev/api/films/${query.get("films")}`)
+fetch(api)
     .then(response => {
         return response.json()
     }).then(parsedResponse => {
-        console.log(parsedResponse)
         const urls = parsedResponse.results.map(result => result.url)
         const fetches = urls.map(url => fetch(url).then(response => response.json()))
         return Promise.all(fetches)
     }).then(responses => {
+        responses[0].poster = "https://m.media-amazon.com/images/M/MV5BNzVlY2MwMjktM2E4OS00Y2Y3LWE3ZjctYzhkZGM3YzA1ZWM2XkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_SX300.jpg"
+        responses[1].poster = "https://m.media-amazon.com/images/M/MV5BYmU1NDRjNDgtMzhiMi00NjZmLTg5NGItZDNiZjU5NTU4OTE0XkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_SX300.jpg"
+        console.log(responses)
         spinner.classList.add("hidden")
         responses.forEach(response => {
             addFilm(response)
