4917ba98d8244512a5a4be3feb908193208fa284
Blair Bristow <blairhbristow@gmail.com>
2022-04-05 11:24:40 -0400

    Add css

---
+++ site/index.js
@@ -0,0 +1,28 @@
+const api = "https://swapi.dev/api/films/"
+const spinner = document.querySelector(".spinner")
+const movieListings = document.querySelector("#movie-listings")
+
+
+function addFilm(film) {
+    const div = document.createElement("div")
+    div.classList.add("movie-listing")
+    div.innerHTML = `
+    <a href="swapi.dev?film=${film.title}">${film.title}</a>
+    <time>${film.release_date}</time>
+    `
+    movieListings.append(div)
+}
+
+fetch(api)
+    .then(response => {
+        return response.json()
+    }).then(parsedResponse => {
+        const urls = parsedResponse.results.map(result => result.url)
+        const fetches = urls.map(url => fetch(url).then(response => response.json()))
+        return Promise.all(fetches)
+    }).then(responses => {
+        spinner.classList.add("hidden")
+        responses.forEach(response => {
+            addFilm(response)
+        })
+    })
