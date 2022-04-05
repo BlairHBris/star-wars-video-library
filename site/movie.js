694e9c904adabff9bbb15d272acd763bf5461b7e
Sharron <sking171@ford.com>
2022-04-05 12:59:02 -0500

    Test movie.js

--- site/movie.js
+++ site/movie.js
@@ -5,16 +5,19 @@ const swapi = "https://swapi.dev/api/films/"
 
 
 
-fetch(swapi)
-    .then(response => {
-        return response.json()
-    }).then(parsedResponse => {
-        const urls = parsedResponse.results.map(result => result.url)
-        const fetches = urls.map(url => fetch(url).then(response => response.json()))
-        return Promise.all(fetches)
-    }).then(responses => {
-        spinner.classList.add("hidden")
-        responses.forEach(response => {
-            addFilm(response)
-        })
-    })
+// fetch(swapi)
+//     .then(response => {
+//         return response.json()
+//     }).then(parsedResponse => {
+//         const urls = parsedResponse.results.map(result => result.url)
+//         const fetches = urls.map(url => fetch(url).then(response => response.json()))
+//         return Promise.all(fetches)
+//     }).then(responses => {
+//         spinner.classList.add("hidden")
+//         responses.forEach(response => {
+//             addFilm(response)
+//         })
+//     })
+
+fetch(`https://swapi.dev/api/films/${query.get("film")}`)
+const query = new URLSearchParams(url.search)
