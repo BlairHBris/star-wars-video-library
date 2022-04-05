46ff51f5ce786ac4cbabae4f5acf9dd7f643316c
Blair Bristow <blairhbristow@gmail.com>
2022-04-05 15:38:06 -0400

    Edit api to only be one

--- site/index.js
+++ site/index.js
@@ -1,68 +1,35 @@
 const api = "https://swapi.dev/api/films/"
 const spinner = document.querySelector(".spinner")
 const movieListings = document.querySelector(".movie-listings")
-const newHope = "http://www.omdbapi.com/?t=star+wars&apikey=b5743b67"
-const empire = "http://www.omdbapi.com/?t=empire+strikes+back&apikey=b5743b67"
-const returnOfTheJedi = "http://www.omdbapi.com/?t=return+of+the+jedi&apikey=b5743b67"
-const phantom = "http://www.omdbapi.com/?t=phantom+menace&apikey=b5743b67"
-const clones = "http://www.omdbapi.com/?t=Star+Wars%3A+Episode+II-+Attack+of+the+Clones&apikey=b5743b67"
-const revenge = "http://www.omdbapi.com/?t=revenge+of+the+sith&apikey=b5743b67"
-
-
-
-
 
 function addFilm(film) {
     const div = document.createElement("div")
     div.classList.add("movie-listing")
     div.innerHTML = `
-    <img src="${film.Poster}" alt="${film.Title}"/>
-    <a href="movie.html?movie=${film.Title}">${film.Title}</a>
-    <time>${film.Year}</time>
+    <img src="${film.poster}" alt="${film.title}"/>
+    <a href="movie.html?movie=${film.title}">${film.title}</a>
+    <time>${film.release_date}</time>
     `
     movieListings.append(div)
 }
 
-fetch(newHope)
+fetch(api)
     .then(response => {
         return response.json()
-    }).then(response => {
+    }).then(parsedResponse => {
+        const urls = parsedResponse.results.map(result => result.url)
+        const fetches = urls.map(url => fetch(url).then(response => response.json()))
+        return Promise.all(fetches)
+    }).then(responses => {
+        responses[0].poster = "https://m.media-amazon.com/images/M/MV5BNzVlY2MwMjktM2E4OS00Y2Y3LWE3ZjctYzhkZGM3YzA1ZWM2XkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_SX300.jpg"
+        responses[1].poster = "https://m.media-amazon.com/images/M/MV5BYmU1NDRjNDgtMzhiMi00NjZmLTg5NGItZDNiZjU5NTU4OTE0XkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_SX300.jpg"
+        responses[2].poster = "https://m.media-amazon.com/images/M/MV5BMTBhNDhiOWYtNTc2NC00ZmRiLWI5ZGQtNDhmNTkxOGYyZWNhXkEyXkFqcGdeQXVyMTEwNDU1MzEy._V1_SX300.jpg"
+        responses[3].poster = "https://m.media-amazon.com/images/M/MV5BYTRhNjcwNWQtMGJmMi00NmQyLWE2YzItODVmMTdjNWI0ZDA2XkEyXkFqcGdeQXVyNTAyODkwOQ@@._V1_SX300.jpg"
+        responses[4].poster = "https://m.media-amazon.com/images/M/MV5BMDAzM2M0Y2UtZjRmZi00MzVlLTg4MjEtOTE3NzU5ZDVlMTU5XkEyXkFqcGdeQXVyNDUyOTg3Njg@._V1_SX300.jpg"
+        responses[5].poster = "https://m.media-amazon.com/images/M/MV5BNTc4MTc3NTQ5OF5BMl5BanBnXkFtZTcwOTg0NjI4NA@@._V1_SX300.jpg"
+        console.log(responses)
         spinner.classList.add("hidden")
-        addFilm(response)
-    })
-
-fetch(empire)
-    .then(response => {
-        return response.json()
-    }).then(response => {
-        spinner.classList.add("hidden")
-        addFilm(response)
-    })
-fetch(returnOfTheJedi)
-    .then(response => {
-        return response.json()
-    }).then(response => {
-        spinner.classList.add("hidden")
-        addFilm(response)
-    })
-fetch(phantom)
-    .then(response => {
-        return response.json()
-    }).then(response => {
-        spinner.classList.add("hidden")
-        addFilm(response)
-    })
-fetch(clones)
-    .then(response => {
-        return response.json()
-    }).then(response => {
-        spinner.classList.add("hidden")
-        addFilm(response)
-    })
-fetch(revenge)
-    .then(response => {
-        return response.json()
-    }).then(response => {
-        spinner.classList.add("hidden")
-        addFilm(response)
+        responses.forEach(response => {
+            addFilm(response)
+        })
     })
