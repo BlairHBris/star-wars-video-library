4912b165ffe5e76470683ca42394494d6c7418e3
Blair Bristow <blairhbristow@gmail.com>
2022-04-05 17:03:17 -0400

    Add content

--- site/movie.js
+++ site/movie.js
@@ -5,49 +5,69 @@ const movieDiv = document.querySelector(".movie")
 const ul = document.querySelector(".characters")
 const api = "https://swapi.dev/api/films/"
 
+const episodeIDMap = {
+    1: 4,
+    2: 5,
+    3: 6,
+    4: 1,
+    5: 2,
+    6: 3
+}
+
 function addFilm(film) {
     movieDiv.innerHTML = `
     <img src="${film.poster}" alt="${film.title}"/>
     <a href="movie.html?movie=${film.title}">${film.title}</a>
     <time>${film.release_date}</time>
+    <div>${film.opening_crawl}</div>
     `
-    movieListings.append(div)
 }
-const movieTitle = queryString.get("movie")
-console.log(movieTitle)
+
+function addCharacters(character) {
+    const li = document.createElement("li")
+    li.innerHTML = `${character.name}`
+    ul.append(li)
+}
+
+const movieID = episodeIDMap[queryString.get("movie")]
+console.log(movieID)
 
 
-fetch(`https://swapi.dev/api/films/${movieTitle}`)
+fetch(`https://swapi.dev/api/films/${movieID}`)
     .then(response => {
-        console.log(response)
-        if (movieTitle == 4) {
-            response.poster = "https://m.media-amazon.com/images/M/MV5BNzVlY2MwMjktM2E4OS00Y2Y3LWE3ZjctYzhkZGM3YzA1ZWM2XkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_SX300.jpg"
-        }
-        if (movieTitle == 1) {
-            response.poster = "https://m.media-amazon.com/images/M/MV5BYTRhNjcwNWQtMGJmMi00NmQyLWE2YzItODVmMTdjNWI0ZDA2XkEyXkFqcGdeQXVyNTAyODkwOQ@@._V1_SX300.jpg"
-        }
-
         return response.json()
     }).then(parsedResponse => {
+        if (movieID == 1) {
+            parsedResponse.poster = "https://m.media-amazon.com/images/M/MV5BNzVlY2MwMjktM2E4OS00Y2Y3LWE3ZjctYzhkZGM3YzA1ZWM2XkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_SX300.jpg"
+        }
+        if (movieID == 2) {
+            parsedResponse.poster = "https://m.media-amazon.com/images/M/MV5BYmU1NDRjNDgtMzhiMi00NjZmLTg5NGItZDNiZjU5NTU4OTE0XkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_SX300.jpg"
+        }
+        if (movieID == 3) {
+            parsedResponse.poster = "https://m.media-amazon.com/images/M/MV5BMTBhNDhiOWYtNTc2NC00ZmRiLWI5ZGQtNDhmNTkxOGYyZWNhXkEyXkFqcGdeQXVyMTEwNDU1MzEy._V1_SX300.jpg"
+        }
+        if (movieID == 4) {
+            parsedResponse.poster = "https://m.media-amazon.com/images/M/MV5BYTRhNjcwNWQtMGJmMi00NmQyLWE2YzItODVmMTdjNWI0ZDA2XkEyXkFqcGdeQXVyNTAyODkwOQ@@._V1_SX300.jpg"
+        }
+        if (movieID == 5) {
+            parsedResponse.poster = "https://m.media-amazon.com/images/M/MV5BMDAzM2M0Y2UtZjRmZi00MzVlLTg4MjEtOTE3NzU5ZDVlMTU5XkEyXkFqcGdeQXVyNDUyOTg3Njg@._V1_SX300.jpg"
+        }
+        if (movieID == 6) {
+            parsedResponse.poster = "https://m.media-amazon.com/images/M/MV5BNTc4MTc3NTQ5OF5BMl5BanBnXkFtZTcwOTg0NjI4NA@@._V1_SX300.jpg"
+        }
         addFilm(parsedResponse)
+        console.log(parsedResponse)
+        const charactersRequests = parsedResponse.characters
+            .map(url => {
+                return fetch(url).then(response => response.json())
+            })
+        return Promise.all(charactersRequests)
+    }).then(parsedResponses => {
+        spinner.classList.add("hidden")
+        parsedResponses.forEach(parsedResponse => {
+            addCharacters(parsedResponse)
+        })
     })
 
 
 
-/*
-    const urls = parsedResponse.results.map(result => result.url)
-    const fetches = urls.map(url => fetch(url).then(response => response.json()))
-    return Promise.all(fetches)
-}).then(responses => {
-    responses[0].poster = "https://m.media-amazon.com/images/M/MV5BNzVlY2MwMjktM2E4OS00Y2Y3LWE3ZjctYzhkZGM3YzA1ZWM2XkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_SX300.jpg"
-    responses[1].poster = "https://m.media-amazon.com/images/M/MV5BYmU1NDRjNDgtMzhiMi00NjZmLTg5NGItZDNiZjU5NTU4OTE0XkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_SX300.jpg"
-    responses[2].poster = "https://m.media-amazon.com/images/M/MV5BMTBhNDhiOWYtNTc2NC00ZmRiLWI5ZGQtNDhmNTkxOGYyZWNhXkEyXkFqcGdeQXVyMTEwNDU1MzEy._V1_SX300.jpg"
-    responses[3].poster = "https://m.media-amazon.com/images/M/MV5BYTRhNjcwNWQtMGJmMi00NmQyLWE2YzItODVmMTdjNWI0ZDA2XkEyXkFqcGdeQXVyNTAyODkwOQ@@._V1_SX300.jpg"
-    responses[4].poster = "https://m.media-amazon.com/images/M/MV5BMDAzM2M0Y2UtZjRmZi00MzVlLTg4MjEtOTE3NzU5ZDVlMTU5XkEyXkFqcGdeQXVyNDUyOTg3Njg@._V1_SX300.jpg"
-    responses[5].poster = "https://m.media-amazon.com/images/M/MV5BNTc4MTc3NTQ5OF5BMl5BanBnXkFtZTcwOTg0NjI4NA@@._V1_SX300.jpg"
-    console.log(responses)
-    spinner.classList.add("hidden")
-    responses.forEach(response => {
-        addFilm(response)
-    })
-})*/
